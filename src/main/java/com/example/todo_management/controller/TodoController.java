a1f295ce7fb72233c3b05038437e9e472f910ad4
Mohan Raj <mohanmwraj@gmail.com>
2025-10-16 12:48:34 +0530

    Added Exception, end-points for the todo's

--- src/main/java/com/example/todo_management/controller/TodoController.java
+++ src/main/java/com/example/todo_management/controller/TodoController.java
@@ -5,14 +5,14 @@ import com.example.todo_management.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/todos")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class TodoController {
 
     private TodoService todoService;
@@ -24,5 +24,46 @@ public class TodoController {
         return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
     }
 
+    // Build Get Todo REST API
+    @GetMapping("{id}")
+    public ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId){
+        TodoDto todoDto = todoService.getTodo(todoId);
+        return new ResponseEntity<>(todoDto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoDto>> getAllToDo(){
+        List<TodoDto> todos = todoService.getAllTodos();
+        //return new ResponseEntity<>(todos, HttpStatus.OK);
+
+        return ResponseEntity.ok(todos);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TodoDto> updateTodo(
+                                                @RequestBody TodoDto todoDto,
+                                                @PathVariable("id") Long todoId
+                                        ){
+        TodoDto updatedDto = todoService.updateTodo(todoDto, todoId);
+        return new ResponseEntity<>(updatedDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteTodo(@PathVariable("id") Long todoId) {
+        todoService.deleteTodo(todoId);
+        return ResponseEntity.ok("Todo deleted successfully!.");
+    }
+
+    @PatchMapping("{id}/complete")
+    public ResponseEntity<TodoDto> completeToDo(@PathVariable("id") Long todoId){
+        TodoDto updatedDto = todoService.completeTodo(todoId);
+        return ResponseEntity.ok(updatedDto);
+    }
+
+    @PatchMapping("{id}/in-complete")
+    public ResponseEntity<TodoDto> inCompleteToDo(@PathVariable("id") Long todoId){
+        TodoDto updatedDto = todoService.inCompleteTodo(todoId);
+        return ResponseEntity.ok(updatedDto);
+    }
 
 }
--- src/main/java/com/example/todo_management/service/Impl/TodoServiceImpl.java
+++ src/main/java/com/example/todo_management/service/Impl/TodoServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.todo_management.service.Impl;
 
 import com.example.todo_management.dto.TodoDto;
 import com.example.todo_management.entity.Todo;
+import com.example.todo_management.exception.ResourceNotFoundException;
 import com.example.todo_management.repository.TodoRepository;
 import com.example.todo_management.service.TodoService;
 import lombok.AllArgsConstructor;
@@ -45,31 +46,62 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public TodoDto getTodo(Long id) {
-        return null;
+
+        Todo todo = todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " +id));
+
+        return modelMapper.map(todo, TodoDto.class);
     }
 
     @Override
     public List<TodoDto> getAllTodos() {
-        return List.of();
+
+        List<Todo> todos = todoRepository.findAll();
+
+        return todos.stream()
+                .map((todo) -> modelMapper.map(todo, TodoDto.class))
+                .toList();
     }
 
     @Override
     public TodoDto updateTodo(TodoDto todoDto, Long id) {
-        return null;
+      Todo todo = todoRepository.findById(id)
+              .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
+
+      todo.setTitle(todoDto.getTitle());
+      todo.setDescription(todo.getDescription());
+      todo.setCompleted(todo.isCompleted());
+
+      Todo updatedTodo = todoRepository.save(todo);
+      return modelMapper.map(updatedTodo, TodoDto.class);
     }
 
     @Override
     public void deleteTodo(Long id) {
+        Todo todo = todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
 
+        todoRepository.deleteById(id);
     }
 
     @Override
     public TodoDto completeTodo(Long id) {
-        return null;
+        Todo todo = todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
+
+        todo.setCompleted(Boolean.TRUE);
+        Todo updatedTodo = todoRepository.save(todo);
+
+        return modelMapper.map(updatedTodo, TodoDto.class);
     }
 
     @Override
     public TodoDto inCompleteTodo(Long id) {
-        return null;
+        Todo todo = todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
+
+        todo.setCompleted(Boolean.FALSE);
+        Todo updatedTodo = todoRepository.save(todo);
+        return modelMapper.map(updatedTodo, TodoDto.class);
     }
 }
