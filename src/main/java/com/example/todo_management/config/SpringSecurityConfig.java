1ab84e9bf10d3dadeb381f9f3f0f5972e9ec9577
Mohan Raj <mohanmwraj@gmail.com>
2025-10-16 16:46:26 +0530

    Added Role Based Autherization

--- src/main/java/com/example/todo_management/config/SpringSecurityConfig.java
+++ src/main/java/com/example/todo_management/config/SpringSecurityConfig.java
@@ -2,8 +2,8 @@ package com.example.todo_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.PasswordManagementDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,9 +24,27 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-//        http.csrf().disable() #marked for removal
+//        http.csrf().disable() #marked for removal in latest version
               http.authorizeHttpRequests((authorize) -> {
-                    authorize.anyRequest().authenticated();
+                          // Authorize POST method when only having admin access
+                          authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+
+                          // Authorize PUT method when only having admin access
+                          authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+
+                          // Authorize DELETE method when only having admin access
+                          authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+
+                          //Both User Admin Roles user can access GET details
+                          authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+
+                          //Both User Admin Roles user can do Partial update using PATCH method.
+                          authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN", "USER");
+
+//                          // GET request make publicly accessible.
+//                          authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
+
+                          authorize.anyRequest().authenticated();
                 })
                 .httpBasic(Customizer.withDefaults());
 
