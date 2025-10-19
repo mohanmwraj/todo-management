d56ffdcf9867c396ddcb87bd1080146d62d6ebcb
Mohan Raj <mohanmwraj@gmail.com>
2025-10-19 06:07:10 +0530

    Method Level security POST method having issue

--- src/main/java/com/example/todo_management/config/SpringSecurityConfig.java
+++ src/main/java/com/example/todo_management/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SpringSecurityConfig {
 
     @Bean
@@ -21,36 +23,37 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
-//        http.csrf().disable() #marked for removal in latest version
-              http.authorizeHttpRequests((authorize) -> {
-                          // Authorize POST method when only having admin access
-                          authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
-
-                          // Authorize PUT method when only having admin access
-                          authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
-
-                          // Authorize DELETE method when only having admin access
-                          authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
-
-                          //Both User Admin Roles user can access GET details
-                          authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
-
-                          //Both User Admin Roles user can do Partial update using PATCH method.
-                          authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN", "USER");
-
-//                          // GET request make publicly accessible.
-//                          authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
-
-                          authorize.anyRequest().authenticated();
-                })
-                .httpBasic(Customizer.withDefaults());
-
-
-        return http.build();
-    }
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//
+////        http.csrf().disable() #marked for removal in latest version
+//              http.authorizeHttpRequests((authorize) -> {
+//                          // Authorize POST method when only having admin access
+////                          authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+////
+////                          // Authorize PUT method when only having admin access
+////                          authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+////
+////                          // Authorize DELETE method when only having admin access
+////                          authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+////
+////                          //Both User Admin Roles user can access GET details
+////                          authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+////
+////                          //Both User Admin Roles user can do Partial update using PATCH method.
+////                          authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN", "USER");
+//
+////                          // GET request make publicly accessible.
+//                          authorize.requestMatchers( "/api/**").permitAll();
+//
+//                          authorize.anyRequest().
+//                                  authenticated();
+//                })
+//                .httpBasic(Customizer.withDefaults());
+//
+//
+//        return http.build();
+//    }
 
     @Bean
     public UserDetailsService userDetailsService(){
--- src/main/java/com/example/todo_management/controller/TodoController.java
+++ src/main/java/com/example/todo_management/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.example.todo_management.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class TodoController {
     private TodoService todoService;
 
     //Build Add Todo REST API
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
         TodoDto savedTodo =todoService.addTodo(todoDto);
@@ -25,12 +27,14 @@ public class TodoController {
     }
 
     // Build Get Todo REST API
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId){
         TodoDto todoDto = todoService.getTodo(todoId);
         return new ResponseEntity<>(todoDto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping
     public ResponseEntity<List<TodoDto>> getAllToDo(){
         List<TodoDto> todos = todoService.getAllTodos();
@@ -39,6 +43,7 @@ public class TodoController {
         return ResponseEntity.ok(todos);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<TodoDto> updateTodo(
                                                 @RequestBody TodoDto todoDto,
@@ -48,18 +53,21 @@ public class TodoController {
         return new ResponseEntity<>(updatedDto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteTodo(@PathVariable("id") Long todoId) {
         todoService.deleteTodo(todoId);
         return ResponseEntity.ok("Todo deleted successfully!.");
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/complete")
     public ResponseEntity<TodoDto> completeToDo(@PathVariable("id") Long todoId){
         TodoDto updatedDto = todoService.completeTodo(todoId);
         return ResponseEntity.ok(updatedDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/in-complete")
     public ResponseEntity<TodoDto> inCompleteToDo(@PathVariable("id") Long todoId){
         TodoDto updatedDto = todoService.inCompleteTodo(todoId);
